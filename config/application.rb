069cd46bc6a8c6802198a5ff4eac5d6e61d1e7f1
goPaddle <testgopaddle@gmail.com>
2016-06-27 17:08:38 +0530

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module PollApi
 	config.middleware.insert_before 0, "Rack::Cors" do
 		allow do
 			origins '*'
-			resource '*', :headers => :any, :methods => [:get, :post, :patch, :options]
+			resource '*', :headers => :any, :methods => [:get, :post, :patch, :delete, :options]
 		end
 	end
   end
