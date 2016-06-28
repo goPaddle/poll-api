c7404147a235fb377b315b9929629dba8815ab1a
goPaddle <testgopaddle@gmail.com>
2016-06-28 12:31:44 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 	namespace :api, :defaults => {:format => :json} do
 		resources(:users, :only => [:create])
 		resources(:sessions, :only => [:create])
-		resources(:polls, :only => [:index, :show, :create, :destroy]) do
+		resources(:polls, :only => [:index, :show, :create]) do
 			resources(:votes, :only => [:create])
 		end
 	end
